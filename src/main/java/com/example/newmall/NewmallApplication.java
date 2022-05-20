b783fea25d0af33189915607975173259a0ff1e7
charliezong <charliezong18@gmail.com>
2022-05-19 20:46:42 -0400

    Init push

---
+++ src/main/java/com/example/newmall/NewmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.newmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  NewmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newmall/NewmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
